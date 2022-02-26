232536786bfd2c7b12998bceec05297943fa667a
Amro Al Salih <amro5698@gmail.com>
2022-02-26 01:01:42 -0500

    NextUI Chat

--- frontend/helpdesk/pages/index.js
+++ frontend/helpdesk/pages/index.js
@@ -2,6 +2,8 @@ import Head from 'next/head';
 import { useRouter } from 'next/router';
 import uuid from 'react-uuid';
 
+import { Card, Grid, Col, Row, Text, Button, Spacer } from '@nextui-org/react';
+
 export default function Home() {
 
   const router = useRouter();
@@ -20,12 +22,12 @@ export default function Home() {
   }
 
   return (
-    <div>
-      <Head>
-        <title>HelpDesk</title>
-      </Head>
-      <h1>Welcome to Help Desk</h1>
-      <button onClick={CreateChat}>New Chat</button>
-    </div >
+    <Row justify="center">
+      <Button onClick={CreateChat} size="lg">
+        <Text css={{ color: 'inherit' }} size={12} weight="bold" transform="uppercase">
+          Start Chatting Annonymously
+        </Text>
+      </Button>
+    </Row>
   );
 }
