3f77ef21c4a64d035a45c13a119da410e01a4e84
Amro Al Salih <amro5698@gmail.com>
2022-02-26 16:07:07 -0500

    Final Chat UI

--- frontend/helpdesk/pages/index.js
+++ frontend/helpdesk/pages/index.js
@@ -1,9 +1,7 @@
-import Head from 'next/head';
+import React from 'react';
 import { useRouter } from 'next/router';
 import uuid from 'react-uuid';
-
-import { Card, Grid, Col, Row, Text, Button, Spacer } from '@nextui-org/react';
-
+import { Container, Input, Mail, Row, Text, Button, Modal, Password, Checkbox } from '@nextui-org/react';
 export default function Home() {
 
   const router = useRouter();
@@ -20,14 +18,83 @@ export default function Home() {
     ).then(router.push(`/chat/${newUuid}`));
 
   }
+  const [visible, setVisible] = React.useState(false);
+  const handler = () => setVisible(true);
+  const closeHandler = () => {
+    setVisible(false);
+    console.log('closed');
+  };
 
   return (
-    <Row justify="center">
-      <Button onClick={CreateChat} size="lg">
-        <Text css={{ color: 'inherit' }} size={12} weight="bold" transform="uppercase">
-          Start Chatting Annonymously
-        </Text>
-      </Button>
-    </Row>
+    <Container>
+      <Row justify="center">
+        <Button
+          onClick={CreateChat}
+          size="lg"
+        >
+          <Text css={{ color: 'inherit' }} size={12} weight="bold" transform="uppercase">
+            Start Chatting Annonymously
+          </Text>
+        </Button>
+      </Row>
+      {/* <Row>
+          <Button auto shadow onClick={handler}>
+            Open modal
+          </Button>
+          <Modal
+            closeButton
+            aria-labelledby="modal-title"
+            open={visible}
+            onClose={closeHandler}
+          >
+            <Modal.Header>
+              <Text id="modal-title" size={18}>
+                Welcome to
+                <Text b size={18}>
+                  NextUI
+                </Text>
+              </Text>
+            </Modal.Header>
+            <Modal.Body>
+              <Input
+                clearable
+                bordered
+                fullWidth
+                color="primary"
+                size="lg"
+                placeholder="Email"
+                contentLeft={<Mail />}
+              />
+              <Input
+                clearable
+                bordered
+                fullWidth
+                color="primary"
+                size="lg"
+                placeholder="Password"
+                contentLeft={<Password />}
+              />
+              <Row justify="space-between">
+                <Checkbox>
+                  <Text size={14}>
+                    Remember me
+                  </Text>
+                </Checkbox>
+                <Text size={14}>
+                  Forgot password?
+                </Text>
+              </Row>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button auto flat color="error" onClick={closeHandler}>
+                Close
+              </Button>
+              <Button auto onClick={closeHandler}>
+                Sign in
+              </Button>
+            </Modal.Footer>
+          </Modal>
+        </Row> */}
+    </Container>
   );
 }
