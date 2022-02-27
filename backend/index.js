9e4c1b1a57126a7a5f2c642dfd1f898a02ee33d7
Reuben <intelphysic@gmail.com>
2022-02-26 20:36:08 -0500

    created registration and login routes for help agent auth

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,22 @@ const express = require("express");
 const http = require("http");
 const cors = require("cors");
 const { Server } = require("socket.io");
+const redis = require("redis");
+const e = require("express");
 
 const app = express();
+const client = redis.createClient({
+  url: process.env.REDIS_URL
+});
+
+client.on('error', err => {
+  console.log('Error ' + err);
+});
+
+// express middleware
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 let roomStore = [];
 
@@ -43,6 +56,43 @@ app.get('/', (req, res) => {
   })
 });
 
+app.get('/login', async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  await client.get(username).then(storedPassword => {
+    if (storedPassword == null) {
+      res.json({
+        authenticated: false,
+        reason: "No user found"
+      });
+      return;
+    }
+    if (storedPassword == password) {
+      res.json({
+        authenticated: true
+      });
+      return;
+    } else {
+      res.json({
+        authenticated: false,
+        reason: "Incorrect password"
+      });
+      return;
+    }
+  });
+});
+
+app.post('/register', async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  if (await client.exists(username)) {
+    res.sendStatus(403);
+  } else {
+    await client.set(username, password);
+    res.sendStatus(200);
+  }
+})
+
 app.post('/:room', (req, res) => {
   console.log("Creating room", req.params.room);
   roomStore.push(req.params.room);
@@ -57,4 +107,5 @@ app.delete('/:room', (req, res) => {
 
 server.listen(8080, () => {
   console.log("Server is running...");
+  client.connect();
 });
