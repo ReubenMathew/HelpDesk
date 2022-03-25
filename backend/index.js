e76a79c915b579de4a5b6901a8a2de8eaa39392d
Reuben <intelphysic@gmail.com>
2022-03-25 19:54:31 -0400

    server-side cors rules

--- backend/index.js
+++ backend/index.js
@@ -24,6 +24,12 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 let roomStore = [];
 
