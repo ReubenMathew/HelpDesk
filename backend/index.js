40893fbe4fd7cbdb1326717e47ffeb87c9961806
Reuben <intelphysic@gmail.com>
2022-03-26 21:59:21 -0400

    access control allow origin hardcode

--- backend/index.js
+++ backend/index.js
@@ -31,6 +31,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://help-desk-608pyv7nu-reubenmathew.vercel.app");
   res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST", "DELETE");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
