f4adeefda6b09dec0c62c38b05b8d36c858e6a0b
Reuben <intelphysic@gmail.com>
2022-03-26 22:23:25 -0400

    logging

--- frontend/helpdesk/pages/index.js
+++ frontend/helpdesk/pages/index.js
@@ -33,7 +33,7 @@ export default function Home() {
       .then(res => res)
       .then(res => {
         //TODO(Reuben): Remove in production
-        console.log("Response headers", res.headers)
+        console.log("Response headers", res.headers.keys())
         console.log("COOKIES:", cookies);
         fetch(
           `${process.env.BACKEND_URL}/${newUuid}`,
